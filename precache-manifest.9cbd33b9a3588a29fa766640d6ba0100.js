a8e5d2500e8e80fc9334f12a5c40f16b9f14783b
Rolanda Azeem <azeemrolanda@gmail.com>
2020-10-08 08:11:02 +0000

    Updates

---
+++ precache-manifest.9cbd33b9a3588a29fa766640d6ba0100.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f019382685d1f0c231824bbe3c9f7a22",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "87bc7a95c948220aea14",
+    "url": "/weather-app/static/css/main.ebc01ff8.chunk.css"
+  },
+  {
+    "revision": "03bf537e541804c1f166",
+    "url": "/weather-app/static/js/2.dd531f59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.dd531f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87bc7a95c948220aea14",
+    "url": "/weather-app/static/js/main.a36e2877.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "dab8e41b606bcd932b9f337cb120c241",
+    "url": "/weather-app/static/media/download.dab8e41b.jpg"
+  },
+  {
+    "revision": "f854d08aa411ce0f6c503316e103c399",
+    "url": "/weather-app/static/media/logo.f854d08a.png"
+  }
+]);
