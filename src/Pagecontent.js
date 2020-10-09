cb7385546c277b8390cd473cb78f9c162c61749d
Rolanda Azeem <azeemrolanda@gmail.com>
2020-10-09 11:22:13 +0000

    update 1

--- src/Pagecontent.js
+++ src/Pagecontent.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import download from './download.jpg';
+import React, {useState} from 'react';
 import './App.css';
 
 function Pagecontent() {
+
+    const[cityname, setCityname] = useState('Kasoa');
   return (
     <div className="App-Content">
       <h3>What is the weather like in . . .</h3>
 
-      <form className="App-City-Btn">
-        <input type="text" placeholder="Enter a City here"/>
-        <br></br>
-        <button class="btn btn-success" type="submit">Submit</button>
-    </form>
+        <form className="App-City-Btn">
+          <input onChange={e => setCityname( e.target.value)} type="text" placeholder="Enter a City here"/>
+          <br></br>
+          <h>{cityname}</h>
+          <br></br>
+          <button class="btn btn-success" type="submit">Submit</button>
+        </form>
     </div>
   );
 }
